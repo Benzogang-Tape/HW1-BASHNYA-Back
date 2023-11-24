3ee0aea9c50f16129978dcb8792b245c2116372a
DJ Tape <Dj.Tapee@yandex.ru>
2023-11-24 23:45:07 +0300

    Solve task-3

--- task-3/main.go
+++ task-3/main.go
@@ -3,10 +3,23 @@ package main
 import "fmt"
 
 func main() {
-	var n int
+	var n uint
+	var temp int
 	fmt.Scan(&n)
-	for i := 0; i < n; i++ {
-
+	arr := make([]int, n)
+	for i := 0; uint(i) < n; i++ {
+		var elem int
+		fmt.Scan(&elem)
+		arr[i] = elem
 	}
 
+	temp = arr[n-1]
+	for i := 1; i < int(n); i++ {
+		arr[int(n)-i] = arr[int(n)-1-i]
+	}
+	arr[0] = temp
+
+	for _, elem := range arr {
+		fmt.Printf("%d ", elem)
+	}
 }
