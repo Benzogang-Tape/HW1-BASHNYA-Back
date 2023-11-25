a5ea3ca83bd00095996f664e38300d7881ec2d15
DJ Tape <Dj.Tapee@yandex.ru>
2023-11-25 21:18:48 +0300

    Solve task-5 and task-6

--- task-5/main.go
+++ task-5/main.go
@@ -1,7 +1,25 @@
 package main
 
-import "fmt"
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"strings"
+)
 
 func main() {
-	fmt.Println()
+	str := bufio.NewScanner(os.Stdin)
+	str.Scan()
+	res_str := str.Text()
+	res_str = strings.Replace(res_str, "1", "one", -1)
+	fmt.Println(res_str)
 }
+
+//ans := strings.Split(res_str, "")
+/*
+for i, value := range res_str {
+		if value == 49 {
+			res_str = strings.Replace(res_str, "1", "one", 1)
+		}
+		fmt.Println(i, value)
+	}*/
--- task-6/main.go
+++ task-6/main.go
@@ -1,7 +1,29 @@
 package main
 
-import "fmt"
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"strconv"
+	"strings"
+)
+
+type dictionary map[int]struct{}
+
+func (dict dictionary) add(elem int) {
+	dict[elem] = struct{}{}
+}
 
 func main() {
-	fmt.Println()
+	set := dictionary{}
+	str := bufio.NewScanner(os.Stdin)
+	str.Scan()
+	res_str := str.Text()
+	slc := strings.Split(res_str, " ")
+
+	for _, value := range slc {
+		ans, _ := strconv.Atoi(value)
+		set.add(ans)
+	}
+	fmt.Println(len(set))
 }
